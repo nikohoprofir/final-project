eb0616c5911532ca82f9320ecf2476fd1791d34b
nikohoprofir <nikohoprofir@gmail.com>
2025-03-07 07:35:50 +0000

    Add 2.rs

---
+++ src/2.rs
@@ -0,0 +1,10 @@
+  fn main() {
+    let x = 5;
+    let y = 10;
+    
+    if (x > y) {
+        println!("{} is greater than {}", x, y);
+    } else {
+        println!("{} is less than or equal to {}", x, y);
+    }
+}
