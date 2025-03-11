6c4d03f746e56ce4901466a06926319f3e25c5d7
nikohoprofir <nikohoprofir@gmail.com>
2025-03-11 15:40:24 +0000

    Add 6.rs

---
+++ src/6.rs
@@ -0,0 +1,7 @@
+// Random Rust Code Generated with Deep Learning
+
+fn main() {
+    let mut rng = rand::thread_rng();
+    let random_number: u8 = rng.gen();
+    println!("Random Number: {}", random_number);
+}
