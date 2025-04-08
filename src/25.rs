1bc1c392710bef43e3c66f153f32fe44391d8e38
nikohoprofir <nikohoprofir@gmail.com>
2025-04-08 06:41:50 +0000

    Add 25.rs

---
+++ src/25.rs
@@ -0,0 +1,11 @@
+fn main() {
+    // Example Rust code
+    let mut array = Vec::new();
+    array.push(1);
+    array.push(2);
+    array.push(3);
+    
+    for i in 0..array.len() {
+        println!("{}", array[i]);
+    }
+}
